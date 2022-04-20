02d999c7cd59377195004f5c3d94221f62ab435f
Meenssi <fumikoe439@gmail.com>
2022-04-20 11:28:38 +0700

    Update nodeserver.js

--- nodeserver.js
+++ nodeserver.js
@@ -1,9 +1,17 @@
-const express = require('express')
+const express = require('express') //install express
 const app = express()
 const port = 8080
+
+//Define Variables
+var id = 65140322;
+var status = "single";
+
+//set & call EJS
+app.set('view engine','ejs')
+
 //Creating App --> Pointer(=>{Object})
 app.get("/",(req,rea) =>{
-    res.send("Hello NodeJS!")
+    res.send("Hello NodeJS Test!")
 })
 //Open Server
 app.listen(port,() =>{
